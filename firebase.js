e39b9388a5e0cec0743468cd19a509320f7228b7
Likpalmo marius N'KOUBA <nkoubamarius@gmail.com>
2020-12-17 20:07:41 +0100

    Initialisation du projet et creation des Fichiers de la section Header

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDUaH74yNazp3Dd0FgR20OVSXGmFKKbdFI",
+  authDomain: "linkedin-clone-f9174.firebaseapp.com",
+  projectId: "linkedin-clone-f9174",
+  storageBucket: "linkedin-clone-f9174.appspot.com",
+  messagingSenderId: "189578387974",
+  appId: "1:189578387974:web:1730e766289c67994e5987",
+  measurementId: "G-VKHYPQWT1T",
+};
