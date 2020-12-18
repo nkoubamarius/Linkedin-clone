f08163542310c1739edc98cb5152c06f69bd7971
Likpalmo marius N'KOUBA <nkoubamarius@gmail.com>
2020-12-18 13:10:48 +0100

    Ajout des Fichier du Component Login

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Feed from "./components/Feed/Feed";
 import Widgets from "./components/Widgets/Widgets";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import Login from "./components/Login/Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Sidebar />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
